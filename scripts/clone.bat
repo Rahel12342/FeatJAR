ce39bf5452f453057b8d1794925ad8859058a84d
Rahel <rahel.arens@uni-ulm.de>
2023-08-02 13:37:16 +0200

    Updates repo txt

--- scripts/clone.bat
+++ scripts/clone.bat
@@ -1,6 +1,6 @@
 :<<BATCH
     @echo off
-    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone %%A
+    for /F "tokens=*" %%A in (scripts/repo.txt) do git clone --branch sample_reduction %%A
     exit /b
 BATCH
-xargs -L1 git clone --progress < scripts/repo.txt
+xargs -L1 git clone --progress --branch sample_reduction < scripts/repo.txt
